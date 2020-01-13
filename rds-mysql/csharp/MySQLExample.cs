8d6ec476ee282d43b16dc8222d2ba712da6f0382
倚贤 <yixian.dw@alibaba-inc.com>
2020-01-13 20:58:27 +0800

    partly refact csharp

---
+++ rds-mysql/csharp/MySQLExample.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Aliyun.Serverless.Core;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+namespace FcExample
+{
+    public class MysqlExample
+    {
+        private static String host = Environment.GetEnvironmentVariable("MYSQL_HOST");
+        private static String port = Environment.GetEnvironmentVariable("MYSQL_PORT");
+        private static String dbName = Environment.GetEnvironmentVariable("MYSQL_DBNAME");
+        private static String user = Environment.GetEnvironmentVariable("MYSQL_USER");
+        private static String passwd = Environment.GetEnvironmentVariable("MYSQL_PASSWORD");
+        private static string connStr = $"Host={host};Port={port};Username={user};Password={passwd};Database={dbName}";
+
+        public void Initializer(IFcContext context)
+        {
+            MySqlConnection myConn = null;
+            try
+            {
+                myConn = new MySqlConnection(connStr);
+                myConn.Open();
+
+                MySqlCommand myCmd = new MySqlCommand(@"CREATE TABLE IF NOT EXISTS users (
+                    id  VARCHAR(64) NOT NULL,
+                    name    VARCHAR(128) NOT NULL,
+                    PRIMARY KEY(id))", myConn);
+                myCmd.ExecuteNonQuery();
+
+                Console.WriteLine("表创建成功！");
+
+                myCmd.Dispose();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (myConn != null)
+                {
+                    myConn.Close();
+                }
+            }
+
+        }
+        public Stream Handler(Stream input, IFcContext context)
+        {
+            MySqlConnection myConn = null;
+            try
+            {
+                myConn = new MySqlConnection(connStr);
+                myConn.Open();
+                MySqlCommand myCmd = new MySqlCommand("REPLACE INTO users (`id`,`name`) values('5','csharp')", myConn);
+                myCmd.ExecuteNonQuery();
+                myCmd.Dispose();
+
+                myCmd = new MySqlCommand("SELECT * FROM USER", myConn);
+                MySqlDataReader rdr = myCmd.ExecuteReader();
+
+                String names = "";
+                while(rdr.Read()){
+                    names += $", {rdr.GetString(1)}";
+                }
+                Console.WriteLine(names);
+                myCmd.Dispose();
+
+                return new MemoryStream(Encoding.UTF8.GetBytes(names));
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (myConn != null)
+                {
+                    myConn.Close();
+                }
+            }
+        }
+
+    }
+}
