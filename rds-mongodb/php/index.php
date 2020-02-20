e6b31e3ab9c11dafecb8bc924c48a52805d37793
倚贤 <yixian.dw@alibaba-inc.com>
2020-02-21 01:24:43 +0800

    fix mongodb php example

--- rds-mongodb/php/index.php
+++ rds-mongodb/php/index.php
@@ -3,18 +3,21 @@
 require 'vendor/autoload.php';
 function handler($event, $context)
 {
+
     $db_name = $_ENV['MONGO_DATABASE'];
     $uri = $_ENV['MONGO_URL'];
 
     $client = new MongoDB\Client($uri);
 
-    $collection = $client[$db_name]['fc_col'];
+    $collection = $client->$db_name->fc_col;
     
     $result = $collection->insertOne(['DEMO' => 'FC', 'MSG' => 'Hello FunctionCompute For MongoDB']);
     echo "Inserted with Object ID '{$result->getInsertedId()}'", "\n";
-    $result = $collection->find(['DEMO' => 'FC']);
-    foreach ($result as $entry) {
-        echo $entry->_id, ': ', $entry->name, "\n";
+    $cursor = $collection->find(['DEMO' => 'FC']);
+    $result = '';
+    foreach ($cursor as $entry) {
+        echo json_encode($entry->getArrayCopy()), "\n";
+        $result = $result . json_encode($entry->getArrayCopy()) . "\n";
     }
     return $result;
 }
